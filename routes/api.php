89f0db5668e71f158b1c2339df0a691717c1afa4
BrayanPisuna <brayanpisuna@gmail.com>
2020-07-18 22:17:09 -0500

    #15 #22 #29 Rutas

--- routes/api.php
+++ routes/api.php
@@ -1,5 +1,7 @@
 <?php
 
+use App\Order;
+use App\Service;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
@@ -14,6 +16,50 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::middleware('auth:api')->get('/user', function (Request $request) {
+/*Route::middleware('auth:api')->get('/user', function (Request $request) {
     return $request->user();
-});
+});*/
+
+
+/*---------------ORDER---------------------------*/
+Route::get('orders', function() {
+    return Order::all(); });
+
+Route::get('orders/{id}', function($id) {
+    return Order::find($id); });
+
+Route::post('orders', function(Request $request) {
+    return Order::create($request->all()); });
+
+Route::put('orders/{id}', function(Request $request, $id) {
+    $order = Order::findOrFail($id);
+    $order->update($request->all());
+
+    return $order; });
+
+Route::delete('orders/{id}', function($id) {
+
+    Order::find($id)->delete();
+
+    return 204; });
+
+/*---------------SERVICE---------------------------*/
+Route::get('services', function() {
+    return Service::all(); });
+Route::get('services/{id}', function($id) {
+    return Service::find($id); });
+
+Route::post('services', function(Request $request) {
+    return Service::create($request->all()); });
+
+Route::put('services/{id}', function(Request $request, $id) {
+    $service = Service::findOrFail($id);
+    $service->update($request->all());
+
+    return $service; });
+
+Route::delete('services/{id}', function($id) {
+
+    Service::find($id)->delete();
+
+    return 204; });
