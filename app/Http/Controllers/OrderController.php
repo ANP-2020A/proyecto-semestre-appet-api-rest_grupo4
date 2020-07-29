0b40f4eaadac6cd3028d566b419e2fca07353cb0
BrayanPisuna <brayanpisuna@gmail.com>
2020-07-28 21:43:26 -0500

    #39 Recursos Api

--- app/Http/Controllers/OrderController.php
+++ app/Http/Controllers/OrderController.php
@@ -3,6 +3,8 @@
 namespace App\Http\Controllers;
 
 use App\Order;
+use App\Http\Resources\Order as OrderResources;
+use App\Http\Resources\OrderCollection;
 use Illuminate\Http\Request;
 use JWTAuth;
 
@@ -10,22 +12,28 @@ class OrderController extends Controller
 {
     public function index()
     {
-        return Order::all();
+
+        return new OrderCollection(Order::paginate(10));
     }
+
     public function show(Order $order)
     {
-        return $order;
+
+        return response()->json(new OrderResources($order), 200);
     }
+
     public function store(Request $request)
     {
         $order = Order::create($request->all());
         return response()->json($order, 201);
     }
+
     public function update(Request $request, Order $order)
     {
         $order->update($request->all());
         return response()->json($order, 200);
     }
+
     public function delete(Order $order)
     {
         $order->delete(); return response()->json(null, 204);
