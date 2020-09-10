14da9ab756b03d5f673986d92f29ac59fd1329dc
Emerick95 <ahilton.guaras@epn.edu.ec>
2020-09-09 21:36:13 -0500

    Â#72 Modificacion CORS

--- app/Http/Middleware/Cors.php
+++ app/Http/Middleware/Cors.php
@@ -15,9 +15,15 @@ class Cors
      */
     public function handle($request, Closure $next)
     {
-        $request->header('Access-Control-Allow-Origin', 'http://localhost:3000');
-        $request->header('Access-Control-Allow-Methods', 'GET, POST, PUT, PATCH, DELETE, OPTIONS');
-        $request->header('Access-Control-Allow-Headers', 'Origin, Content-Type,X-Requested-With,Content-Type, Authorization');
-        return $next($request);
+        return $next($request)
+            ->header('Access-Control-Allow-Origin', '*')
+            ->header('Access-Control-Allow-Origin', 'http://localhost:3000')
+            ->header('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, OPTIONS')
+            ->header('Access-Control-Allow-Headers', 'X-Requested-With, Content-Type, X-Token-Auth, Authorization')
+            ->header('Access-Control-Allow-Headers', 'Origin, Content-Type, X-Requested-With, Content-Type, X-Token-Auth, Authorization');
+            //->header('Access-Control-Allow-Origin', 'http://localhost:3000');
+            //->header('Access-Control-Allow-Methods', 'GET, POST, PUT, PATCH, DELETE, OPTIONS');
+            //->header('Access-Control-Allow-Headers', 'Origin, Content-Type,X-Requested-With,Content-Type, Authorization');
+
     }
 }
